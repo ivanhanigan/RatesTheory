02e8600d69af8bff1d5ac5fdf48437cf901faf30
ivanhanigan <ivan.hanigan@gmail.com>
2012-08-17 17:06:08 +1000

    init fully

--- init.r
+++ init.r
@@ -1,5 +1,25 @@
 
+###########################################################################
+# newnode: init
+sink('config/global.dcf')
+print("data_loading: on
+cache_loading: on
+munging: on
+logging: off
+load_libraries: off
+libraries: reshape, plyr, ggplot2, stringr, lubridate, epitools, foreign
+as_factors: on
+data_tables: off")
+sink()
+if (!require(reshape)) install.packages('reshape', repos='http://cran.csiro.au'); require(reshape)
+if (!require(plyr)) install.packages('plyr', repos='http://cran.csiro.au'); require(plyr)
+if (!require(ggplot2)) install.packages('ggplot2', repos='http://cran.csiro.au'); require(ggplot2)
+if (!require(stringr)) install.packages('stringr', repos='http://cran.csiro.au'); require(stringr)
+if (!require(lubridate)) install.packages('lubridate', repos='http://cran.csiro.au'); require(lubridate)
+if (!require(epitools)) install.packages('epitools', repos='http://cran.csiro.au'); require(epitools)
+if (!require(foreign)) install.packages('foreign', repos='http://cran.csiro.au'); require(foreign)
+
 ####
 # init additional directories for project management
-source('~/dropbox/tools/analysisTemplate.r')
+source('~/Dropbox/tools/analysisTemplate.r')
 analysisTemplate()
--- munge/func-general.r
+++
@@ -1,5 +0,0 @@
-
-#######################################################################
-# name: func-general
- if (!require(epitools)) install.packages('epitools', repos='http://cran.csiro.au'); require(epitools)
- if (!require(foreign)) install.packages('foreign', repos='http://cran.csiro.au'); require(foreign)
