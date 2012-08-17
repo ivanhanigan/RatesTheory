520c38a64c69f01e5149093124979a98c322cdbf
ivanhanigan <ivan.hanigan@gmail.com>
2012-08-17 16:53:18 +1000

    tangled dstdize and istdize

---
+++ munge/func-general.r
@@ -0,0 +1,5 @@
+
+#######################################################################
+# name: func-general
+ if (!require(epitools)) install.packages('epitools', repos='http://cran.csiro.au'); require(epitools)
+ if (!require(foreign)) install.packages('foreign', repos='http://cran.csiro.au'); require(foreign)
